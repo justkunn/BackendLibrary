41af46e28cd7f43aceb3fbbe0a1bec4dfdc23253
justkunn <iqbalsetiawan536@gmail.com>
2025-12-29 10:21:37 +0700

    add cors to vercel

--- src/main/java/com/example/library/Config/CorsConfig.java
+++ src/main/java/com/example/library/Config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // React dev server
+                .allowedOrigins("http://localhost:5173","https://frontend-library-phi.vercel.app") // React dev server
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
